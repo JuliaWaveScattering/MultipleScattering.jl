a6773a2ada62cc38a2d0da51a90a40e809d154a9
ArtGower <arturgower@gmail.com>
2017-07-14 14:58:04 +0100

    restructered response.jl

--- src/response.jl
+++ src/response.jl
@@ -3,11 +3,42 @@
 function response{T}(model::FrequencyModel{T}, k::T)
 
     response_matrix = build_response_matrix(model, k)
-    # the matrix of scattering coefficient mat is given by mat[m,p] = Zn_vec[m]*A_mat[m,p]
+    # the matrix of scattering coefficient mat is given by mat[m,p] = Zn_vec[m,p]*A_mat[m,p]
     # where mat[m+hankel_order+1,p] is the coefficient for hankelh1[m, kr] of the p-th particle[:,p]
-    #(the 1.0+0.0im is the incident wave when emitted and measured at listener_positions)
-    return one(Complex{T}) + scattered_wave(model, k, response_matrix)
+
+    return [response_at(model, k, response_matrix, model.listener_positions[:,i]) for i in 1:size(model.listener_positions,2)]
 end
+
+"a scattered wave is the total response minus the incident"
+function response_at{T}(model::FrequencyModel{T}, k::T, response_matrix::Matrix{Complex{T}}, x::Vector{T})
+    # check if x is inside a scatterer
+    inside_p(p) = norm(x - p.x) + 10 * eps(T) < p.r
+    if findfirst(inside_p, model.particles) ==0
+        # one(Complex{T}) is the incident wave when emitted and measured at x
+        return one(Complex{T}) + scattered_waves_at(model, k::T, response_matrix, x::Vector{T})
+    else
+        warn("in the middle of implementing internal waves")
+        return zero(Complex{T})
+        # return internal_wave_at(model, k::T x::Vector{T})
+    end
+end
+
+function scattered_waves_at{T}(model, k::T, response_matrix::Matrix{Complex{T}}, x::Vector{T})
+    Nh = model.hankel_order
+    krs = [k*norm(x - p.x) for p in model.particles]
+    θs  = [atan2(x[2] - p.x[2], x[1] - p.x[1]) for p in model.particles]
+
+    scattered_from(i) = sum(
+        m -> response_matrix[m+Nh+1, i] * hankelh1(m, krs[i]) * exp(im * m * θs[i])
+    , -Nh:Nh)
+
+    #Sum wave scattered from each particle then divide by the incident wave
+    return sum(scattered_from, eachindex(model.particles)) * exp(-im * k * dot(x, model.source_direction))
+end
+
+# "the internal wave exists only within a particle and is the total response"
+# function internal_wave(model::FrequencyModel{T}, k::T, response_matrix::Matrix{Complex{T}})
+# end
 
 function build_response_matrix{T}(model::FrequencyModel{T}, k::T)
     # Generate response for one specific k
@@ -51,51 +82,13 @@ function build_response_matrix{T}(model::FrequencyModel{T}, k::T)
     # Vector which represents the ncident wave in k direction i.e. exp(im* dot(k,x)) for each basis function
     ψ = reshape([exp(im * (k * dot(p.x, model.source_direction) + m * pi / 2)) for m=-Nh:Nh, p in model.particles], B)
 
-
     # the scattering coefficients A_mat in matrix, A[1,2] multiplies hankelh1(1, k*r) contributing to the 2nd particle scattered wave
     A_mat = reshape(-(L_mat + eye(B)) \ ψ, (H, P))
 
     return Zn_mat .* A_mat
 end
 
-"a scattered wave is the total response minus the incident"
-function scattered_wave{T}(model::FrequencyModel{T}, k::T, response_matrix::Matrix{Complex{T}})
-    # Number of particles
-    P = length(model.particles)
-    # Highest hankel order used
-    Nh = model.hankel_order
-    # Number of hankel basis function at each particle
-    H = 2Nh + 1
-    # Total hankel basis functions in whole model
-    B = H * P
-
-    #scattered wave from particles[p] measured at x
-    function outwave(i::Int, x::Vector{T})
-        p = model.particles[i]
-        kr = k*norm(x - p.x)
-        θ = atan2(x[2] - p.x[2], x[1] - p.x[1])
-        resp_m(m) = response_matrix[m+Nh+1, i] * hankelh1(m, kr) * exp(im * m * θ)
-        return sum(resp_m, -Nh:Nh)
-    end
-
-    function scattered_at(x)
-        # Check it isn't inside a particle
-        for p in model.particles
-            if(norm(x - p.x) + 10 * eps(T) < p.r )
-                warn("Listener at $x is inside the scatterer centred at $(p.x) with radius $(p.r), this has not been implemented")
-                return -one(Complex{T})
-            end
-        end
-        # If measuring point is within particle then return zero else divide all the scattered waves measured at X by the incident wave
-        outwave_at_x(i) = outwave(i, x)
-        return sum(outwave_at_x,1:P) * exp(-im * k * dot(x, model.source_direction))
-    end
-
-    [scattered_at(model.listener_positions[:,i]) for i in 1:size(model.listener_positions,2)]
-end
-
 "Returns a ratio used in multiple scattering which reflects the material properties of the particles"
-# function Zn{T}(ak::T, m::Int, q::Complex{T}=Complex{T}(Inf), γ::T=one(T))
 function Zn{T}(model::FrequencyModel{T}, p::Particle{T}, k::T,  m::Int)
     m = abs(m)
     ak = p.r*k
