587838d144294cd9553d8424fbaa95f8aa83b4df
Art Gower <arturgower@gmail.com>
2018-02-20 15:20:24 +0000

    readme for lens example

--- example/lens/README.md
+++ example/lens/README.md
@@ -1,1 +1,9 @@
 # Lens
+
+the code [lens.jl](lens.jl) arranges particles into the shape of lens. The lens is shaped so that the incident plane wave is completely focused into one point: the listener position.
+
+```julia
+include("lens.jl")
+plot_lens()
+```
+![Plot lens shape and response in time](plot_lens.png)
--- example/lens/lens.jl
+++ example/lens/lens.jl
@@ -2,14 +2,14 @@ using MultipleScattering
 using Plots
 
 function run_lens(;
-        a=1.0,
+        a=0.25,
         volfrac=0.2,
         listener_position=[-10.0;0.0],
-        outertime=30.0,
-        innertime=20.0
+        outertime=35.0,
+        innertime=34.0
     )
 
-    k_arr=collect(linspace(0.01,1.0,100))
+    k_arr=collect(linspace(0.01,2.0,100))
 
     # Generate particles which are at most outertime away from our listener
     outershape = TimeOfFlight(listener_position,outertime)
@@ -20,15 +20,15 @@ function run_lens(;
     particles = filter(p -> !inside(innershape,p), outerparticles)
 
     freqsimulation = FrequencySimulation(particles, k_arr; listener_positions=listener_position)
-
-    return freqsimulation, TimeSimulation(freqsimulation; impulse = get_gaussian_freq_impulse(1.0, 3./maximum(k_arr)^2))
+    time_arr = 0.:0.5:90.
+    return freqsimulation, TimeSimulation(freqsimulation; time_arr=time_arr, impulse = get_gaussian_freq_impulse(1.0, 4.5/maximum(k_arr)^2))
 end
 
 function plot_lens()
-    freqsimulation, TimeSimulation = run_lens()
-
-    plot(
-        plot(freqsimulation.particles),
-        plot(TimeSimulation)
-    )
+    simulation, time_sim = run_lens()
+    plot()
+    plot!.(simulation.particles);
+    p = plot!();
+    xticks = [0.,20.,34.,40.0,60.,80.]
+    plot(p,plot(time_sim, title="", label="", xticks=xticks))
 end
