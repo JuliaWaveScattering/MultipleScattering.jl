b01a21a025ee7ad823135e6fccf0268407715242
jondea <jondea@thedeakin.net>
2018-04-02 01:14:47 +0100

    Small fixes and imporved comments

--- src/particles.jl
+++ src/particles.jl
@@ -1,6 +1,10 @@
 
-type Particles{P,T,Dim} where P <: PhysicalProperties{T}
+abstract PhysicalProperties{T,Dim,FieldDim} where T <: AbstractFloat, Dim::Integer, FieldDim::Integer end
+
+type Particle{T,Dim,P,S} where P <: PhysicalProperties{T,Dim,FieldDim}, S <: Shape{T, Dim}
     position::MVector{T,Dim}
     medium::P
-    shape::Shape{T}
+    shape::S
 end
+
+CircleParticle{P, T, Dim} = Particle{T, Dim, P, Circle{T, Dim}}
--- src/physics.jl
+++ src/physics.jl
@@ -1,27 +1,38 @@
+"""
+Holds information about the physical properties of the medium, the dimension of
+the field and the number of dimensions it is a function of.
+"""
+abstract PhysicalProperties{T,Dim,FieldDim} where T <: AbstractFloat, Dim::Integer, FieldDim::Integer end
 
-abstract PhysicalProperties{T,Dim,FieldDim} end
-
+"""
+Physical properties for a homogenous isotropic acoustic medium. Produces a
+scalar (1D) field in arbitrary dimensions.
+"""
 type Acoustics{T,Dim} <: PhysicalProperties{T,Dim,1}
-    ρ::T #the medium's density
-    c::Complex{T} #the medium's phase velocity
+    ρ::T # Density
+    c::Complex{T} # Phase velocity
 end
 
-function transmission_coefs(medium::Acoustics{T,Dim}, particle::Acoustics{T,Dim}, ω::T) where T <: AbstractFloat, Dim::Int
-    return #Zns???
+function φ(x::MVector{2,T}, physics::Acoustics{T,2}, ω::T, m::Integer)
+    r = norm(x)
+    θ = atan2(x[2],x[1])
+    k = ω/physics.c
+    hankelh1(k*r,m)*exp(im*θ*m)
 end
 
-# Could this type of framework with multiple dispacth be used in full FSI??
-function transmission_coefs(medium::Acoustics, particle::TimeHarmonicLinearElast)
-
+"""
+Basis functions in a specific dimension for a specific physics type.
+"""
+function φ(x::MVector{Dim,T}, physics::P, ω::T, m::Int) where Dim::Integer, P <: PhysicalProperties{T,Dim,FieldDim}
+    error("Basis functions not implmented for this physics type.")
 end
 
-function φ(x::MVector{2,T}, physics::Acoustics{T,2}, ω::T, m::Int)
-    r = x[1]; θ = x[2]
-    hankelh1(ω/physics.c*r,m)*exp(im*θ*m)
-end
-
-type Electromagnetism{T,Dim} <: PhysicalProperties{T,Dim,1}
+"""
+Physical properties for a homogenous isotropic electromagnetic medium. Produces
+a vector (??) field in arbitrary dimensions.
+"""
+type Electromagnetism{T,Dim} <: PhysicalProperties{T,Dim,3}
     μ::Complex{T} # Permeability
     ε::Complex{T} # Permittivity
-    σ::Complex{T} #Conductivity
+    σ::Complex{T} # Conductivity
 end
