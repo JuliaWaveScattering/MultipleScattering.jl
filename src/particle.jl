6ba1671f768b1c7cab4539c77b89879062938474
jondea <jondea@thedeakin.net>
2018-04-03 00:12:54 +0100

    Delete redundant particle.jl

--- src/particle.jl
+++ src/particle.jl
@@ -6,3 +6,13 @@ type Particle{T,Dim,P,S} where P <: PhysicalProperties{Dim,FieldDim,T}, S <: Sha
 end
 
 CircleParticle{P, T, Dim} = Particle{T, Dim, P, Circle{T}}
+
+volume(p::Particle) = volume(p.shape)
+
+volume(particles::Vector{Particle}) = mapreduce(volume, +, particles)
+
+function ==(p1::Particle, p2::Particle)
+    p1.position == p2.position &&
+    p1.medium == p2.medium &&
+    p1.shape == p2.shape
+end
