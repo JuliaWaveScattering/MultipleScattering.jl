cd8637bf129f350e339d39a6ad420bd61e0c4caa
jondea <jondea@thedeakin.net>
2018-04-03 00:35:55 +0100

    Flesh out shape.jl, add rectangle, volume and name

--- src/shape.jl
+++ src/shape.jl
@@ -1,6 +1,32 @@
-
+"""
+Abstract idea which defines the external boundary of object. Two objects have
+the same shape if they are congruence.
+"""
 abstract type Shape{T,Dim} end
 
+volume(shape::Shape) = error("Volume function not implemented for shape")
+
+name(shape::Shape) = error("Volume function not implemented for shape")
+
+
+"Shape where boundary is fixed distance from a point"
 type Circle{T} <: Shape{T,2}
     radius::T
 end
+
+function volume{T}(shape::Circle{T})
+    return π * shape.radius^2
+end
+
+name(shape::Circle) = "Circle"
+
+
+"Shape with 4 right-angles, defined by a height and width"
+type Rectangle{T} <: Shape{T,2}
+    width::T
+    height::T
+end
+
+function volume(rectangle::Rectangle{T}) = rectangle.width*rectangle.height
+
+name(shape::Rectangle) = "Rectangle"
