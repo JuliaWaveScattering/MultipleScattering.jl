eb1b856b553b1dceb777864a68a41d8e51b7854f
ArtGower <arturgower@gmail.com>
2018-08-10 16:25:44 +0100

    add test for time shift impulse

--- test/time_tests.jl
+++ test/time_tests.jl
@@ -51,3 +51,47 @@ using MultipleScattering
         @test norm(field(timres1) - field(timres2))/norm(field(timres1)) < 2e-5
         # plot(timres1.t', [field(timres1)[1,:]-field(timres2)[1,:]])
 end
+
+@testset "Time shift signal" begin
+
+    sound_sim = Acoustic(2., 5.2 + 0.0im,2)
+    # suppose we measure in time the signal
+    t = 0.1:0.01:30.
+    t0 = 5.
+    impulse_func(t) = exp( -10000.*(t - t0)^4)
+    measured_response = impulse_func.(t)
+    plot(t, measured_response)
+
+    ω = t_to_ω(t)
+    measured_f = time_to_frequency(measured_response, t, ω)
+
+    # at the point
+    source_x = [-3.0,2.0]
+    source_direction = [1.0,1.0]
+    x_measure =  source_x + 4.*source_direction
+
+    # then can we reverse-engineer what plane wave source (of the form below) would create this measured_response
+    travel_time = norm(x_measure - source_x)/real(sound_sim.c)
+    impulse_time = t .- travel_time;
+    impulse_response = impulse_func.(t);
+
+    amp0 = 1.0
+    source = plane_source(sound_sim, source_x, source_direction, amp0)
+    sim = FrequencySimulation(sound_sim, source)
+    simres = run(sim, [x_measure, x_measure + source_direction], ω)
+    short_time = 1.:0.00005:1.5
+    timres = frequency_to_time(simres; t_vec = short_time)
+    i1 = findmax(abs.(field(timres)[1,:]))[2]
+    i2 = findmax(abs.(field(timres)[2,:]))[2]
+    # test wave peak propagation speed
+    @test (short_time[i2] - short_time[i1] - norm(source_direction/sound_sim.c)) < 1e-4*norm(source_direction/sound_sim.c)
+
+    amps = measured_f./field(simres)[1,:]
+
+    # predict the source impulse
+    predict_impulse = frequency_to_time(amps, ω, impulse_time[300:600])
+    # correct mistake from ω=0
+    predict_impulse = predict_impulse .- mean(predict_impulse[1:2])
+
+    @test norm(predict_impulse - impulse_response[300:600]) < 1e-12*norm(impulse_response[300:600])
+end
