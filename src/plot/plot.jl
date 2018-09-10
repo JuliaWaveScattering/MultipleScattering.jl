cf61b3df8be5550a751f4970511f1549226dd27b
arturgower <arturgower@gmail.com>
2018-09-10 17:20:22 +0100

    Moved plots of field to plot_field.
    Also added example of making a harmonic wave into a gif.

--- src/plot/plot.jl
+++ src/plot/plot.jl
@@ -1,4 +1,5 @@
 include("plot_domain.jl")
+include("plot_field.jl")
 
 # Plot the result across angular frequency for a specific position (x)
 @recipe function plot(simres::SimulationResult;
@@ -19,45 +20,10 @@ include("plot_domain.jl")
     end
 end
 
-# Plot the result in space (across all x) for a specific angular frequency
-@recipe function plot(simres::SimulationResult, ω_or_t::AbstractFloat;
-        x_indices = indices(simres.x,1),
-        ω_or_t_index = findmin(abs.(getfield(simres, 3) .- ω_or_t))[2],
-        field_apply = real, seriestype = :surface)
-
-    x = [x[1] for x in simres.x[x_indices]]
-    y = [x[2] for x in simres.x[x_indices]]
-    ω_or_t = getfield(simres, 3)[ω_or_t_index]
-
-    fillcolor --> :pu_or
-    title --> "Field at $(fieldnames(simres)[end])=$ω_or_t"
-    seriestype --> seriestype
-    aspect_ratio --> 1.0
-
-    if seriestype == :contour
-
-        # We should really check here to see if x and y have the right structure
-        x = unique(x)
-        y = unique(y)
-
-        n_x = length(x)
-        n_y = length(y)
-
-        fill --> true
-        x, y, field_apply.(transpose(reshape(field(simres)[x_indices,ω_or_t_index],n_y,n_x)))
-
-    else
-
-        (x, y, field_apply.(field(simres)[x_indices,ω_or_t_index]))
-
-    end
-
-end
-
 "Plot just the particles"
 @recipe function plot(sim::FrequencySimulation; bounds = :none)
 
-    println("Plotting a simulation on its own")
+    # println("Plotting a simulation on its own")
 
     @series begin
         if bounds != :none
@@ -72,8 +38,10 @@ end
 end
 
 "Plot the field for a particular wavenumber"
-@recipe function plot(sim::FrequencySimulation{T}, ω::T; res=10, xres=res, yres=res,
-                         field_apply=real, bounds = :auto, drawparticles=false) where {T}
+@recipe function plot(sim::FrequencySimulation{T}, ω::T;
+            res=10, xres=res, yres=res,
+            field_apply=real, bounds = :auto,
+            drawparticles=false) where {T}
 
     # If user wants us to, generate bounding rectangle around particles
     if bounds == :auto
@@ -95,14 +63,12 @@ end
     p_ylims = plotattributes[:ylims]
     bounds = Rectangle((T(p_xlims[1]),T(p_ylims[1])), (T(p_xlims[2]),T(p_ylims[2])))
 
+    field_sim = run(sim, bounds, [ω]; xres=xres, yres=yres)
+    xy_mat = reshape(field_sim.x, (xres+1, yres+1))
+    x_pixels = [x[1] for x in xy_mat[:,1]]
+    y_pixels = [x[2] for x in xy_mat[1,:]]
+
     @series begin
-
-        field_sim = run(sim, bounds, [ω]; xres=xres, yres=yres)
-
-        xy_mat = reshape(field_sim.x, (xres+1, yres+1))
-
-        x_pixels = [x[1] for x in xy_mat[:,1]]
-        y_pixels = [x[2] for x in xy_mat[1,:]]
 
         # Turn the responses (a big long vector) into a matrix, so that the heatmap will understand us
         response_mat = transpose(reshape(field(field_sim), (xres+1, yres+1)))
---
+++ src/plot/plot_field.jl
@@ -0,0 +1,69 @@
+# Plot the result in space (across all x) for a specific angular frequency
+@recipe function plot(simres::FrequencySimulationResult, ω::AbstractFloat;
+        time = 0.0, # does not except "t" as name of variable..
+        x_indices = indices(simres.x,1),
+        ω_index = findmin(abs.(getfield(simres, 3) .- ω))[2],
+        field_apply = real, seriestype = :surface)
+
+    x = [x[1] for x in simres.x[x_indices]]
+    y = [x[2] for x in simres.x[x_indices]]
+    ω = getfield(simres, 3)[ω_index]
+
+    phase = exp(-im*ω*time)
+
+    color --> :pu_or
+    title --> "Field for ω = $ω"
+    seriestype --> seriestype
+    aspect_ratio --> 1.0
+
+    if seriestype == :contour
+        # We should really check here to see if x and y have the right structure
+        x = unique(x)
+        y = unique(y)
+
+        n_x = length(x)
+        n_y = length(y)
+
+        fill --> true
+        x, y, field_apply.(phase.*transpose(reshape(field(simres)[x_indices,ω_index],n_y,n_x)))
+
+    else
+        (x, y, field_apply.(phase.*field(simres)[x_indices,ω_index]))
+    end
+
+end
+
+# Plot the result in space (across all x) for a specific angular frequency
+@recipe function plot(timres::TimeSimulationResult, t::AbstractFloat;
+        x_indices = indices(timres.x,1),
+        t_index = findmin(abs.(getfield(timres, 3) .- t))[2],
+        field_apply = real, seriestype = :surface)
+
+    x = [x[1] for x in timres.x[x_indices]]
+    y = [x[2] for x in timres.x[x_indices]]
+    t = getfield(timres, 3)[t_index]
+
+    color --> :pu_or
+    title --> "Field for time = $t"
+    seriestype --> seriestype
+    aspect_ratio --> 1.0
+
+    if seriestype == :contour
+
+        # We should really check here to see if x and y have the right structure
+        x = unique(x)
+        y = unique(y)
+
+        n_x = length(x)
+        n_y = length(y)
+
+        fill --> true
+        x, y, field_apply.(transpose(reshape(field(timres)[x_indices,t_index],n_y,n_x)))
+
+    else
+
+        (x, y, field_apply.(field(timres)[x_indices,t_index]))
+
+    end
+
+end
