1a35f88a353e71a15af2f8baeeeb48e199577cbc
pivaps <pspiva1@sheffield.ac.uk>
2021-11-05 13:42:56 +0000

    Fixed the example box_size.jl

--- docs/src/example/box_size/box_size.jl
+++ docs/src/example/box_size/box_size.jl
@@ -1,33 +1,47 @@
 using MultipleScattering
 using Plots; pyplot()
 using Random
+using LinearAlgebra
 
-function box_size_convergence(m=4, volfrac = 0.05,
-    radius = 1.0, times = [20.0,30.0,40.0,50.0,60.0,70.0], k_arr=collect(LinRange(0.01,1.0,100)) )
+function box_size_convergence(  m = 4,
+                                volfrac = 0.05,
+                                radius = 1.0,
+                                times = [20.0,30.0,40.0,50.0,60.0,70.0],
+                                ω = collect(LinRange(0.01,1.0,100))
+                                )
 
-    listener_position = [-10.0,0.0]
-    bigshape = TimeOfFlightPlaneWaveToPoint(listener_position,maximum(times))
+    host_medium = Acoustic(1.0, 1.0, 2)
+    particle_medium = Acoustic(1.0, 0.0, 2)
+    particle_shape = Circle(radius)
+
+    listener_position = [-10.0; 0.0]
+    bigshape = TimeOfFlightPlaneWaveToPoint(listener_position, maximum(times))
 
     seed = MersenneTwister(1).seed
-    allparticles = random_particles(volfrac, radius, bigshape; seed = seed)
+    allparticles = random_particles(particle_medium, particle_shape;
+                                            region_shape = bigshape,
+                                            volume_fraction = volfrac,
+                                            seed = seed
+                                    )
+
+    source =  plane_source( host_medium; position = [0.0, 0.0], direction = [1.0, 0.0] )
 
     simulations = map(times) do t
         println("Calculating response with particles at a maximum of $t away")
-        shape = TimeOfFlightPlaneWaveToPoint(listener_position,t)
-        particles = filter(p->p⊆shape, allparticles)
-        return FrequencySimulation(particles, k_arr; seed=seed, hankel_order=m)
+        shape = TimeOfFlightPlaneWaveToPoint(listener_position, t)
+        particles = filter(p -> p ⊆ shape, allparticles)
+        return run(particles, source, [listener_position], ω; basis_order = m)
     end
 
-    return map(m->TimeSimulation(m;time_arr=LinRange(0.0,100.0,201)),simulations)
+    return map(x -> frequency_to_time(x; t_vec = LinRange(0.0,100.0,201)), simulations)
 end
 
-function plot_box_size_convergence(simulations;times = [20,30,40,50,60,70])
+function plot_box_size_convergence(simulations; times = [20,30,40,50,60,70])
 
-    colors = LinRange(RGB(0.6,1,0.6),RGB(0,0.4,0),length(times))
-
-    plot(xguide ="Time (t)",yguide ="Response")
+    plot(xguide = "Time (t)", yguide ="Response")
     for s in eachindex(simulations)
-        plot!(simulations[s],color=colors[s],label="Box cut at t=$(times[s])")
+        plot!(simulations[s], label = "Box cut at t=$(times[s])")
+        plot!(size=(800,600))
     end
     plot!(title="Time response from random particles from increasingly large boxes")
 end
