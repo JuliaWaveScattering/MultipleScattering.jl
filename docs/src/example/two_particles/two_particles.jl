1b362c659d82fe82f932cbffeeecd9ce3f4aa7be
pivaps <pspiva1@sheffield.ac.uk>
2022-02-17 15:58:55 +0000

    Two particles example fixed

--- docs/src/example/two_particles/two_particles.jl
+++ docs/src/example/two_particles/two_particles.jl
@@ -4,30 +4,27 @@ pyplot()
 
 # define two particles to scatter the wave
 # the first is centered at [1.,-2.], with radius 1.0, sound speed 2. and density 10.
-p1 = Particle([1.,-4.], 1.0; c = 20.0+0.0im, ρ = 10.)
-p2 = Particle([3.,3.],  3.0; c = 1.0+0.0im, ρ = 0.1)
+p1 = Particle(Acoustic(2; c = 20.0, ρ = 10.),Sphere([1.,-4.], 1.0))
+p2 = Particle(Acoustic(2; c = 1.0, ρ = 0.1),Sphere([3.,3.], 3.0))
 particles = [p1,p2]
 
 # specify the angular frequency of the incident wave
 w_arr = collect(0.1:0.01:1.)
-# calculate and plot the frequency response
-simulation = FrequencySimulation(particles, w_arr)
+source = plane_source(Acoustic(1.0, 1.0, 2));
+# calculate and plot the frequency response at x
+x = [[-10.0,0.0]];
+simulation = run(particles, source, x, w_arr)
 plot(simulation)
-# the above plot used the default reciever/listener position is [-10.0; 0.0]
-simulation.listener_positions
-# and incident plane in the x direction
-simulation.source_direction
+# the above plot used the reciever/listener position is [-10.0, 0.0] and incident plane wave in the default position, [0.0,0.0], and direction direction, [1.0,0.0]
 # to change these defaults use
-simulation = FrequencySimulation(particles, w_arr;
-    listener_positions = [-10.,-10.],
-    source_direction=[1.,1.])
+x = [[-10.0,-10.0]]
+source = plane_source(Acoustic(1.0, 1.0, 2); direction = [1.0,1.0], position = [0.0,0.0]);
+simulation = run(particles, source, x, w_arr)
 
 # to plot the frequency response over a region that includes the particles
+# Define region to plot
+region = Box([[-11.;-11.], [6.;6.]])
+# Define frequency, run the simulation and plot the field
 w = 3.2
-plot(simulation,w; res=80, field_apply=abs)
-# the green circle in the plot is the reciever position
-
-TimeSimulation = TimeSimulation(simulation)
-plot(TimeSimulation)
-
-# simulation = FrequencySimulation(particles, w_arr; source_direction = [0.,1.], shape = Box([[0.,-10.],[10.,40.]]))
+result = run(particles, source, region, [w]; res=80)
+plot(result, w; field_apply=abs, seriestype = :contour)
